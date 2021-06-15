487de2d7ff881725491ea6724f3e068d207d2b1e
MachikoFujii <machiko.fujii@gmail.com>
2021-06-15 15:55:01 +0900

    Created posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'posts#index'
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/_form'
+  root 'users#index'
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
+  devise_for :users
+  resources :users
+  resources :posts
 end
