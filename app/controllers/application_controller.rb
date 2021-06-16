0644841515758d91feaef06a4970407d52f624f8
MachikoFujii <machiko.fujii@gmail.com>
2021-06-16 09:38:44 +0900

    added name and image columns to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   def check_guest
     email = resource&.email || params[:user][:email].downcase
     if email == 'guest_user@example.com'
