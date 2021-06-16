d5fbcd0428649aecf2c2e9532e4fd54544366649
MachikoFujii <machiko.fujii@gmail.com>
2021-06-17 01:31:53 +0900

    favorit list

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :check_guest, only: %i[update destroy]
+
+  def favorites_index
+    @user = current_user
+    @favorites = @user.favorites
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
