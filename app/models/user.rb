ab2e743abf63204b97a93724e5e6187ce582ff73
MachikoFujii <machiko.fujii@gmail.com>
2021-06-18 12:42:09 +0900

    associated labelings, labels, users tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  has_many :labelings, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_065443) do
+ActiveRecord::Schema.define(version: 2021_06_18_033758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,21 @@ ActiveRecord::Schema.define(version: 2021_06_17_065443) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "labelings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_labelings_on_post_id"
+    t.index ["user_id"], name: "index_labelings_on_user_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -76,4 +91,6 @@ ActiveRecord::Schema.define(version: 2021_06_17_065443) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
+  add_foreign_key "labelings", "posts"
+  add_foreign_key "labelings", "users"
 end
