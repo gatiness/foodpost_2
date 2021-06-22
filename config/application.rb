73e070387826f0dfe260aeb3b923f5d2aff69340
MachikoFujii <machiko.fujii@gmail.com>
2021-06-22 17:12:19 +0900

    prepared factorybot for users.rb

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,23 @@
-require_relative 'boot'
+class Application < Rails::Application
+  require_relative 'boot'
+  require 'rails/all'
+  Bundler.require(*Rails.groups)
 
-require 'rails/all'
+  config.generators do |g|
+    g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false,
+      request_specs: false
+    g.fixture_replacement :factory_bot, dir: "spec/factories"
+  end
 
-Bundler.require(*Rails.groups)
-
-module Foodpost
-  class Application < Rails::Application
-
-    config.load_defaults 5.2
-    config.i18n.default_locale = :ja
-
+  module Foodpost
+    class Application < Rails::Application
+      config.load_defaults 5.2
+      config.i18n.default_locale = :ja
+    end
   end
 end
