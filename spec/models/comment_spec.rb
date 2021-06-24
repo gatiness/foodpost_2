4f9954591979f76d9fdcf3f82a44ebac22e876a9
MachikoFujii <machiko.fujii@gmail.com>
2021-06-24 11:45:17 +0900

    updated comments.html

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,9 +3,10 @@ RSpec.describe Comment, type: :model do
   describe 'commentモデル機能' do
     context 'バリデーションテスト' do
       it 'コメントが空の場合' do
-        user2 = create(:user2)
-        post3 = create(:post3)
-        comment = Comment.new(content: nil, user_id: user2.id, post_id: post3.id)
+        user2 = create(:admin)
+        user = create(:user)
+        post3 = create(:post3, user_id: user2.id)
+        comment = Comment.new(comment: nil, user_id: user2.id, post_id: post3.id)
         expect(comment).to be_invalid
       end
     end
