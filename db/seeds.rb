94553c0ed4a95bf5864cf94e3c419ec66d001568
MachikoFujii <machiko.fujii@gmail.com>
2021-06-24 17:39:53 +0900

    edited seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,43 +16,47 @@ Label.create!(
   ]
 )
 
-User
+# User
 user1 = User.create!(
   name: "test1",
   email: "test1@example.com",
   password: "password",
   password_confirmation: "password",
-  image: File.open("app/assets/images/user_icons/icon1.png"),
   admin: true
 )
+user1.image.attach(io: File.open('./app/assets/images/user_icons/icon1.png'), filename: 'icon1.png')
+
 user2 = User.create!(
   name: "test2",
   email: "test2@example.com",
   password: "password",
-  password_confirmation: "password",
-  image: File.open("app/assets/images/user_icons/icon2.png"),
+  password_confirmation: "password"
 )
+user2.image.attach(io: File.open('./app/assets/images/user_icons/icon2.png'), filename: 'icon2.png')
+
 user3 = User.create!(
   name: "test3",
   email: "test3@example.com",
   password: "password",
-  password_confirmation: "password",
-  image: File.open("app/assets/images/user_icons/icon3.png"),
+  password_confirmation: "password"
 )
+user3.image.attach(io: File.open('./app/assets/images/user_icons/icon3.png'), filename: 'icon3.png')
+
 user4 = User.create!(
   name: "test4",
   email: "test4@example.com",
   password: "password",
-  password_confirmation: "password",
-  image: File.open("app/assets/images/user_icons/icon4.png"),
+  password_confirmation: "password"
 )
+user4.image.attach(io: File.open('./app/assets/images/user_icons/icon4.png'), filename: 'icon4.png')
+
 user5 = User.create!(
   name: "test5",
   email: "test5@example.com",
   password: "password",
-  password_confirmation: "password",
-  image: File.open("app/assets/images/user_icons/icon5.png"),
+  password_confirmation: "password"
 )
+user5.image.attach(io: File.open('./app/assets/images/user_icons/icon5.png'), filename: 'icon5.png')
 
 user6 = User.create!(
   name: "machiko",
@@ -64,37 +68,44 @@ user6 = User.create!(
   admin: true
 )
 user6.image.attach(io: File.open('./app/assets/images/user_icons/icon6.png'), filename: 'icon6.png')
-Post
+
+# Post
 post1_1 = Post.create!(
   title: "test-title1",
   content: "test_content1",
   image: File.open("app/assets/images/post_images/image1.png"),
-  user_id: user4.id,
+  user_id: user4.id
 )
+post1_1.image.attach(io: File.open('./app/assets/images/post_images/image1.png'), filename: 'image1.png')
+
 post1_2 = Post.create!(
   title: "test-title2",
   content: "test_content2",
-  image: File.open("app/assets/images/post_images/image2.png"),
-  user_id: user2.id,
+  user_id: user2.id
 )
+post1_2.image.attach(io: File.open('./app/assets/images/post_images/image2.png'), filename: 'image2.png')
+
 post1_3 = Post.create!(
   title: "test-title3",
   content: "test_content3",
-  image: File.open("app/assets/images/post_images/image3.png"),
-  user_id: user3.id,
+  user_id: user3.id
 )
+post1_3.image.attach(io: File.open('./app/assets/images/post_images/image3.png'), filename: 'image3.png')
+
 post1_4 = Post.create!(
   title: "test-title4",
   content: "test_content4",
-  image: File.open("app/assets/images/post_images/image4.png"),
-  user_id: user4.id,
+  user_id: user4.id
 )
+post1_4.image.attach(io: File.open('./app/assets/images/post_images/image4.png'), filename: 'image4.png')
+
 post1_5 = Post.create!(
   title: "test-title5",
   content: "test_content5",
-  image: File.open("app/assets/images/post_images/image5.png"),
-  user_id: user5.id,
+  user_id: user5.id
 )
+post1_5.image.attach(io: File.open('./app/assets/images/post_images/image5.png'), filename: 'image5.png')
+
  # お気に入り
 Favorite.create!(
   [
