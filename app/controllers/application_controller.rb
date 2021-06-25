01ddcf2f7501318e2169d99bc973adafcb64fa9f
MachikoFujii <machiko.fujii@gmail.com>
2021-06-26 00:09:38 +0900

    fixed buttons, links and placeholders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,22 @@
 class ApplicationController < ActionController::Base
-  
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image])
-  end
+  protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    user_path(resource)
+    posts_path
   end
-
+  
   def check_guest
     email = resource&.email || params[:user][:email].downcase
     if email == 'guest_user@example.com'
       redirect_to root_path, alert: 'ゲストユーザーは編集・削除できません！'
     end
+  end
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image])
   end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,5 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @favorites = current_user.favorites
@@ -6,12 +7,12 @@ class FavoritesController < ApplicationController
   
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to posts_path, notice: 'お気に入りしました'
+    redirect_to post_path(favorite.post_id), notice: 'お気に入りしました'
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id])
     favorite.destroy
-    redirect_to posts_path, notice: 'お気に入りを解除しました'
+    redirect_to post_path(favorite.post_id), notice: 'お気に入りを解除しました'
   end
 end
