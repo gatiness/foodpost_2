4ee3c8ba18b791580b536cc66a2448478aec13a3
MachikoFujii <machiko.fujii@gmail.com>
2021-06-26 19:53:25 +0900

    changed path of page transition for when logged in as admin guest

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,7 +8,7 @@ class Users::SessionsController < Devise::SessionsController
   def admin_guest_sign_in
     guest_user = User.admin_guest
     sign_in guest_user   # ユーザーをログインさせる
-    redirect_to root_path, notice: 'ログインしました。(管理者ゲスト)'
+    redirect_to posts_path, notice: 'ログインしました。(管理者ゲスト)'
   end
 end
 
